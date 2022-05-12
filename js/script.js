2551c00abf5c27587e10a378ce1f1509647989a5
domi802 <64065747+domi802@users.noreply.github.com>
2022-05-12 16:14:27 +0200

    usunięcie skryptu wyrzucającego błąd cursora

--- js/script.js
+++ js/script.js
@@ -11,26 +11,7 @@ window.onscroll = () =>{
     header.classList.remove('active');
 }
 
-window.onmousemove = (e) =>{
-    cursor1.style.top = e.pageY + 'px';
-    cursor1.style.left = e.pageX + 'px';
-    cursor2.style.top = e.pageY + 'px';
-    cursor2.style.left = e.pageX + 'px';
-}
 
-document.querySelectorAll('a').forEach(links =>{
-
-    links.onmouseenter = () =>{
-        cursor1.classList.add('active');
-        cursor2.classList.add('active');
-    }
-
-    links.onmouseleave = () =>{
-        cursor1.classList.remove('active');
-        cursor2.classList.remove('active');
-    }
-
-});
 document.addEventListener("DOMContentLoaded", function() {
     var backgroundCanvas = document.createElement("div");
     backgroundCanvas.style.cssText = "position: fixed; top: 50%; left: 50%; width: 0; height: 0; z-index: 1; \
